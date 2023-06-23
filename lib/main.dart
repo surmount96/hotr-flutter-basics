df7bf9a96fabb80fc0e92c8a683e6a9390c011c1
Ayodeji Ayanwoye <ayanwoye74@gmail.com>
2023-06-24 00:15:46 +0100

    payment example screen, home example screen, general cleanup of the design and installation of google font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:hotr_flutter_basics/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,93 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'DIGITAL TECH SFG'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(widget.title),
-      // ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15, bottom: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                children: [
-                  const SizedBox(height: 40),
-                  Image.asset('asset/images/header.png'),
-                  const SizedBox(height: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Get Better',
-                            style: GoogleFonts.outfit(fontSize: 60),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            'Returns',
-                            style: GoogleFonts.outfit(fontSize: 60),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    'Invest in the world of leading brands & unlock amazing returns with only three taps.',
-                    style: GoogleFonts.plusJakartaSans(fontSize: 16),
-                  ),
-                ],
-              ),
-              TextButton(
-                  onPressed: () {},
-                  // style: ButtonStyle(ma),
-                  child: Container(
-                    height: 75,
-                    decoration: BoxDecoration(
-                      color: const Color(0xfff202734),
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                    child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Center(
-                          child: Text(
-                            'Get Started',
-                            style: GoogleFonts.outfit(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        )),
-                  )),
-            ],
-          ),
-        ),
-      ),
+      home: const HomeExample(),
     );
   }
 }
---
+++ lib/payment_example.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PaymentExample extends StatelessWidget {
+  const PaymentExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 15, bottom: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                children: [
+                  const SizedBox(height: 40),
+                  Image.asset('asset/images/header2.png'),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Payment Made Easy',
+                    style: GoogleFonts.inter(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Built-in Fingerprint, face recognition and more, keeping you completely safe',
+                    style: GoogleFonts.inter(
+                        fontSize: 17, color: Color(0xfff7C7D81), height: 1.5),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                        onPressed: () {},
+                        // style: ButtonStyle(ma),
+                        child: Container(
+                          height: 65,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Color(0xfffFE7A54)),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Center(
+                                child: Text(
+                                  'Login',
+                                  style: GoogleFonts.inter(
+                                      color: Color(0xfffFE7A54),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              )),
+                        )),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: TextButton(
+                        onPressed: () {},
+                        // style: ButtonStyle(ma),
+                        child: Container(
+                          height: 65,
+                          decoration: BoxDecoration(
+                            color: const Color(0xfffFE7A54),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Center(
+                                child: Text(
+                                  'Register',
+                                  style: GoogleFonts.inter(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              )),
+                        )),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: TextButton(
+                    onPressed: () {},
+                    // style: ButtonStyle(ma),
+                    child: Container(
+                      height: 65,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset('asset/images/google.png'),
+                                const SizedBox(width: 10),
+                                Text(
+                                  'Sign in with google',
+                                  style: GoogleFonts.inter(
+                                      color: Color(0xfff404148),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                          )),
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
